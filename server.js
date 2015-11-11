7ae904f92614ce811df96855bbefd26f057d38d4
murcielago17 <murcielago17@hotmail.com>
2015-11-10 23:39:15 -0800

    add hbs

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
-  app.get('/', function (req, res) {
+  	app.get('/', function (req, res) {
 		res.send('hello world');
 	});
- 
+  	app.get.render('index.html');
 var server = app.listen(process.env.PORT || 3000, function () {
 	console.log('Example app listening at http://localhost:3000/');
 });
