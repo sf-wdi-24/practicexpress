d2574cfb94ec5a9addd84b3e266279c11f97aad7
mrockway <mrockway@gmail.com>
2015-11-10 21:14:18 -0800

    added css,index,scripts

--- nodeProject/server.js
+++ nodeProject/server.js
@@ -1,6 +1,31 @@
+//require express and hbs node packages
 var express = require('express');
 var hbs = require('hbs');
 
 var app = express();
 
-console.log("listening");
+var teams = [{name: "Giants"},
+						 {name: "A's"},
+						 {name: "49ers"}
+];
+
+//enable browser to render hbs pages
+app.set('view engine', 'hbs');
+
+//tell express to look for public folder
+app.use(express.static('public'));
+
+//get request and send it to the browser
+// app.get('/', function(req,res) {
+// 	res.send('String');
+// });
+
+app.get('/', function (req,res) {
+	res.render('index', {teams: teams});
+});
+
+//give server an address
+app.listen(3000, function(){
+	console.log("listening still");
+});
+
